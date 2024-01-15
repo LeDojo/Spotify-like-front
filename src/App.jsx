7cdc7c13b5f5546bdfde2dbe2a27870ac46986d5
Sora <sora@MacBook-Pro-de-Sora.local>
2024-01-15 13:01:34 +0100

    get all the playlists, get one playlist, delete song from playlist, get all songs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [playlists, setPlaylists] = useState([]);
+  const [songs, setSongs] = useState([]);
+  const [playlistTitle, setPlaylistTitle] = useState("All songs");
+  const [playlistId, setPlaylistId] = useState("");
+
+  const fetchPlaylist = (playlistId) => {
+    fetch(`http://localhost:4567/playlists/${playlistId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPlaylistId(data._id);
+        setPlaylistTitle(data.title);
+        setSongs(data.songs);
+      });
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:4567/playlists/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setPlaylists(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:4567/songs/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setSongs(data);
+      });
+  }, []);
+
+  const onPlaylistClick = (playlistId) => {
+    fetchPlaylist(playlistId);
+  };
+
+  const handleDeleteSongFromPlaylist = (songId) => {
+    fetch(
+      `http://localhost:4567/playlists/${playlistId}/delete-song/${songId}`,
+      {
+        method: "PUT",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        fetchPlaylist(playlistId);
+      });
+  };
+
   return (
     <main>
       <div className="sidebar d-flex flex-column flex-shrink-0 p-3 text-white bg-dark">
@@ -15,43 +63,38 @@ function App() {
         <hr />
 
         <ul className="nav nav-pills flex-column mb-auto">
-          <li className="nav-item">
-            <a href="#" className="nav-link active" aria-current="page">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              Orders
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link text-white">
-              Customers
-            </a>
-          </li>
+          {playlists.map((playlist) => (
+            <li
+              key={playlist._id}
+              onClick={() => {
+                onPlaylistClick(playlist._id);
+              }}
+            >
+              <a href="#" className="nav-link text-white">
+                {playlist.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
 
       <div className="main-content">
+        <h1>{playlistTitle}</h1>
         <ul className="list-group">
-          <li className="list-group-item active" aria-current="true">
-            An active item
-          </li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
+          {songs.map((song) => (
+            <li key={song._id} className="list-group-item">
+              <div className="song">
+                <span className="songName">{song.title}</span>
+                <button
+                  onClick={() => {
+                    handleDeleteSongFromPlaylist(song._id);
+                  }}
+                >
+                  ❌
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </main>
